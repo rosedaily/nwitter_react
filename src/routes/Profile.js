eef780f6aa38786fd32949b4c8a24dc1538f8ff0
rose <rosedaily1001@gmail.com>
2022-04-28 06:37:11 +0900

    필터링 기능 추가

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,29 @@
-import { authService } from 'fbase';
+import { authService, dbService } from 'fbase';
+import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-const Profile = () =>{
+const Profile = ({ userObj }) =>{
   const history = useHistory();
 
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+
+  const getMyNweets = async () => {
+    /* 필터링 관련 코드 */
+    const nweets = await dbService
+    .collection("nweets")
+    .where("creatorId","==", userObj.uid)
+    .orderBy("createdAt","asc")
+    .get();
+
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
 
   return (
     <>
