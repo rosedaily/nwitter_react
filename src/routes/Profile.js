ad20ca5c7708217eab01a616841efdcee6412429
rose <rosedaily1001@gmail.com>
2022-04-29 05:14:17 +0900

    프로필페이지 기능 보완

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,48 +1,40 @@
 import { authService, dbService } from 'fbase';
-import { useEffect,useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Nweet from 'components/Nweet';
 
 const Profile = ({ userObj }) =>{
   const history = useHistory();
-  const [nweets, setNweets] = useState([]);
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName)
   
-
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
 
-  const getMyNweets = async () => {
-    /* 필터링 관련 코드 */
-    const nweets = await dbService
-    .collection("nweets")
-    .where("creatorId","==", userObj.uid)
-    .orderBy("createdAt","asc")
-    .get();
-
-    const newArray = nweets.docs.map((document) => ({
-      id: document.id,
-      ...document.data(),
-    }));
-    setNweets(newArray);
+  const onChange = (event) => {
+    const { 
+      target : { value },
+    } = event;
+    setNewDisplayName(value);
   };
 
-  useEffect(() => {
-    getMyNweets();
-  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    // userObj.updateProfile()
+    if(userObj.displayName !== newDisplayName){
+      await userObj.updateProfile({ displayName: newDisplayName});
+    }
+  };
 
   return (
-    <div>
-      <div>
-        {nweets.map((nweet) => (
-          <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid}/>
-        ))}
-      </div>
-      <div>
-        <button onClick={onLogOutClick}>Log Out</button>
-      </div>
-    </div>
+    <>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} type="text" placeholder="Display name" value={newDisplayName}/>
+        <input type="submit" value="Update Profile"/>
+      </form>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
   )
 }
 
